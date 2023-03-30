da2541b636aba5f79bc70a707e03fd9b43484fdb
Stivmartinez <stivmartinez@guruhotel.local>
2023-03-30 18:02:42 -0500

    single and taxonomy conditional

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,16 +1,36 @@
 import { fetchSingle } from "@/api/single"
-
-import { processApiResponse } from "@/lib/processApiResponse"
+import { fetchTaxonomy } from "@/api/taxonomy"
 import BlocksRender from "@/components/blocksRender"
+import { processApiResponse } from "@/lib/processApiResponse"
 
 export default async function Single({ params }: { params: { slug: string } }) {
-  // Get API response
-  const response = await fetchSingle()
-  const { body, data } = await processApiResponse(
+  let response, body, data
+
+  // Fetch single data
+  response = await fetchSingle()
+  const { body: bodySingle, data: dataSingle } = await processApiResponse(
     response,
     params.slug,
     "single"
   )
+
+  const single = dataSingle?.wordpress?.posts?.single
+  const hasSingleData = single && Array.isArray(single) && single.length > 0
+
+  if (hasSingleData) {
+    body = bodySingle
+    data = dataSingle
+  } else {
+    // Fetch taxonomy data
+    response = await fetchTaxonomy()
+    const { body: bodyTax, data: dataTax } = await processApiResponse(
+      response,
+      params.slug
+    )
+
+    body = bodyTax
+    data = dataTax
+  }
 
   return <BlocksRender template={body} data={data} />
 }
