07726c15b29bbb0df60212d10db88cfbf0f2fbfd
Stivmartinez <stivmartinez@guruhotel.local>
2023-03-30 23:29:26 -0500

    pagination block added

--- components/blocks/pagination/index.tsx
+++ components/blocks/pagination/index.tsx
@@ -1,12 +1,47 @@
 "use client"
 
-import { useState } from "react"
+import { useCallback, useState } from "react"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 export default function BlockPagination() {
-  const [posts, setPosts] = useState()
+  const router = useRouter()
+  const [isFetching, setIsFetching] = useState(false)
+  const [page, setPage] = useState(1)
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  const isMutating = isFetching
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set(name, value)
+
+      return params.toString()
+    },
+    [searchParams]
+  )
+
+  function handleLoadMore() {
+    setIsFetching(true)
+
+    // Increment the page number
+    const nextPage = page + 1
+
+    // Update the URL with the new page number
+    const newUrl =
+      pathname + "?" + createQueryString("page", nextPage.toString())
+    router.push(newUrl)
+
+    setIsFetching(false)
+    setPage(nextPage)
+  }
+
   return (
     <div>
-      <button>Holi</button>
+      <button onClick={handleLoadMore} disabled={isFetching}>
+        {isMutating ? "Loading..." : "Load More"}
+      </button>
     </div>
   )
 }
